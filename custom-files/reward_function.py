1f6314f35151f17f24dec5d3963583409bf1b167
adavies48 <73471057+adavies48@users.noreply.github.com>
2023-03-02 20:16:54 +0000

    Update reward_function.py

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,8 +1,10 @@
 def reward_function(params):
-
+    '''
+    Example of rewarding the agent to follow center line
+    '''
     
     # Read input parameters
     speed = params['speed']
-
-
+  
+    
     return float(speed)
